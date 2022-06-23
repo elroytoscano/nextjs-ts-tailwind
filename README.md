e6675261ce95fc556756ac2dca53e454265e8e3e
elroytoscano <andeltos@gmail.com>
2022-06-23 22:26:15 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# NextJS with Typescript and TailwindCSS
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const Home: NextPage = () => {
+  return <div className='h-full w-full bg-zinc-500'>NextJS with Tailwind</div>;
+};
+
+export default Home;
